5d3e2abfc51c0ca2d315f65ae078ad98ae4f5a9f
alexchao26 <alexchaoo@gmail.com>
2019-10-27 15:43:07 -0400

    that's much less janky... one query that adds an address and user at the same time

--- database/seedSQL.js
+++ database/seedSQL.js
@@ -3,49 +3,33 @@ const Pool = require('./dbConnection');
 
 // console.log(typeof Number(faker.address.zipCode()));
 async function seed() {
-  // test inserting into address table
-  const fakerAddressValues = [
+  // create an array of variables to be inserted into the database
+  const values = [
     faker.address.streetAddress(),
     faker.address.secondaryAddress(),
     faker.address.city(),
     faker.address.state(),
     faker.address.zipCode(), // this seems to sometimes throw an error when the column type is INT
-  ];
-
-  let newAddressId;
-
-  // this feels like a very hacky way to do this... there's probably a way to do this with one query
-  // THIS IS ALSO NOT ACID Compliant right now because an error can lead to a null foregin key value
-  await Pool.query(`
-    INSERT INTO addresses(address, "address2", city, state, "zipCode") 
-    VALUES ($1, $2, $3, $4, $5)
-    RETURNING *
-    `, fakerAddressValues)
-    // set the newAddressId variable to this newId to be used when added the next user
-    .then((newRow) => { newAddressId = newRow.rows[0].id; })
-    .catch((err) => console.log('error adding address', err));
-
-  // create an array of variables to be inserted into the database
-  const customerArr = [
     faker.name.firstName(),
     faker.name.lastName(),
     faker.internet.email(),
-    newAddressId,
     faker.phone.phoneNumber(),
   ];
 
-  // console.log(fakerValues.concat(customerArr));
+  // console.log('full input array is', values);
 
   await Pool.query(`
+    WITH newAd AS (
+      INSERT INTO addresses(address, "address2", city, state, "zipCode") 
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *
+    )
     INSERT INTO customers("firstName", "lastName", "email", "addressId", "phoneNumber")
-    VALUES ($1, $2, $3, $4, $5)
+    VALUES ($6, $7, $8, (SELECT id FROM newAd), $9)
     RETURNING *
-    `, customerArr)
-    .then((newRow) => console.log('new customer\'s first name', newRow.rows[0].firstName))
-    .catch((err) => console.log('error adding customer', err));
-
-
-  // await Pool.query('SELECT * FROM addresses').then((data) => console.log(data.rows));
+    `, values)
+    .then((newRow) => console.log('new customer details', newRow.rows))
+    .catch((err) => console.log('ERROR ADDING CUSTOMER AND/OR ADDRESS', err));
 }
 
 // seed a random number of times
